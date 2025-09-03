c570676ec5ad62918db3ae70fa7386e6e02bf89f
Daniel G <danielgz.m@hotmail.com>
2025-09-02 19:02:07 -0500

    form completo

--- js/index.js
+++ js/index.js
@@ -81,4 +81,53 @@ document.addEventListener('DOMContentLoaded', () => {
             icon: 'error'
         });
     }
+
+    //MANEJO INPUT->CARGUE ARCHIVOS
+const inputs = [
+  { inputId: "hoja_vida", spanId: "file-name" },
+  { inputId: "video_presentacion", spanId: "file-video-presentacion" },
+  { inputId: "audio_lectura", spanId: "file-audio-lectura" },
+  { inputId: "audio_comercial", spanId: "file-audio-comercial" }
+];
+
+inputs.forEach(({ inputId, spanId }) => {
+  const input = document.getElementById(inputId);
+  const span = document.getElementById(spanId);
+
+  if (input && span) {
+    input.addEventListener("change", () => {
+      span.textContent = input.files.length > 0
+        ? input.files[0].name
+        : "Ningún archivo seleccionado";
+    });
+  }
+  });
+//FIN CARGUE ARCHIVOS
+
+
+//MANEJO IMAGEN:
+ // Abrir modal al hacer clic en la imagen
+  const img = document.getElementById("lectura-img");
+  const modal = document.getElementById("imgModal");
+  const modalImg = document.getElementById("modalImg");
+  const closeBtn = document.querySelector(".close");
+
+  img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+
+  // Cerrar modal al dar clic en la X
+  closeBtn.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // También cerrar si se hace clic fuera de la imagen
+  modal.onclick = function(e) {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  }
+  //FIN MANEJO IMAGEN
+
 });
